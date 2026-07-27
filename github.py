34273f71b9e181639dc91b2799fd6bb5a9a882e9
tanujadesai1122 <tanujadesai2006@gmail.com>
2026-07-27 15:13:01 +0530

    first commit

---
+++ github.py
@@ -0,0 +1,7 @@
+#  covert text base emotions into emojis
+msg=input("enter your message")
+msg=msg.replace(":)","😊")
+msg=msg.replace(":(","😒")
+msg=msg.replace(":D","😄")
+msg=msg.replace(";)","😉")
+print(msg)
